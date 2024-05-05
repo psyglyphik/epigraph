52c4ef0fa458d99b2fa49994d6837f1fe3a17d47
Adam <adam.h.valiant@gmail.com>
2024-05-05 10:29:57 -0700

    type fix

--- app/board/[boardId]/_components/cursor.tsx
+++ app/board/[boardId]/_components/cursor.tsx
@@ -13,9 +13,10 @@ interface CursorProps {
 export const Cursor = memo(({
   connectionId,
 }: CursorProps) => {
-  const info = useOther(connectionId, (user) => user?.info);
-  const cursor = useOther(connectionId, (user) => user.presence.cursor)
+  const info = useOther(connectionId, (user: any) => user?.info);
+  const cursor = useOther(connectionId, (user: any) => user.presence.cursor)
 
+  
   const name = info?.name || "Teammate";
 
   if (!cursor) {
